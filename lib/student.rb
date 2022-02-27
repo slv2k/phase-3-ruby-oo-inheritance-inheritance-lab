762b3be782d9dfb48f9ad3235e253187ebc512e8
samuel <samuelmmanor@gmail.com>
2022-02-27 09:05:13 -0500

    commit tree df60292288bed464abf8a9526421582690e8dbe3

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,15 @@
-require_relative './user'
-
 class Student < User
-    attr_reader :knowledge
 
     def initialize
         @knowledge = []
     end
 
-    def learn(knowledge)
-        @knowledge << knowledge
+    def learn(str)
+        @knowledge << str
     end
+
+    def knowledge
+        @knowledge
+    end
+
 end
